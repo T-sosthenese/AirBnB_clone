4ecafff2fff125be37030974b01113ffec5442dc
T-sosthenese <sosthenetimi94@gmail.com>
2023-07-12 17:00:05 +0300

    user attributes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class amenity module implementation."""
+from models.base_models import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Amenity inheriting from Class BaseModel."""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization of the class."""
+        super().__init__(*args, **kwargs)
---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Review module implementation."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Class Review inheriting from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Instantiation """
+        super().__init__(*args, **kwargs)
