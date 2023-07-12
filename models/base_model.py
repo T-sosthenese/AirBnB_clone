7278d3a9304bcf81264ce85f5b307684d9914e5a
Congo Justus Migue <congomigue@gmail.com>
2023-07-12 13:49:24 +0000

    #4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,53 @@
-import uuid
+#!/usr/bin/python3
+"""
+This is a module that implements the BaseModel class
+"""
+
+
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    """Base class for all models"""
+    """
+    Base class for all models. A class that defines all common attributes
+    or methods for other classes
+    """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key,datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """
+        Returns the string representation of BaseModel object.
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates 'self_updated_at' with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        data = {
-            'my_number': self.my_number,
-            'name': self.name,
-            '__class__': self.__class__.__name__,
-            'updated_at': self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
-            'id': self.id,
-            'created_at': self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
-        }
-        return data
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance
+        """
+
+        dict_1 = self.__dict__.copy()
+        dict_1["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if k in ("created_at" "updated_at"):
+                v = self.__dict__[k].isoformat()
+                dict_1[k] = v
+        return dict_1
