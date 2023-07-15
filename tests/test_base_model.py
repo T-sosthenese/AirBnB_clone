d1f2181c38a6fa02586ae6d7204e287f41079997
Congo Justus Migue <congomigue@gmail.com>
2023-07-15 10:33:54 +0000

    updated more basemodel.py tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,5 +1,7 @@
 import unittest
+
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
@@ -34,6 +36,31 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn(base_model.id, string_representation)
         self.assertIn("created_at", string_representation)
         self.assertIn("updated_at", string_representation)
+
+    def test_id_uniqueness(self):
+        base_model_1 = BaseModel()
+        base_model_2 = BaseModel()
+        self.assertNotEqual(base_model_1.id, base_model_2.id)
+
+    def test_created_at_type(self):
+        base_model = BaseModel()
+        self.assertIsInstance(base_model.created_at, datetime)
+
+    def test_updated_at_type(self):
+        base_model = BaseModel()
+        self.assertIsInstance(base_model.updated_at, datetime)
+
+    def test_save_updates_updated_at(self):
+        base_model = BaseModel()
+        previous_updated_at = base_model.updated_at
+        base_model.save()
+        self.assertNotEqual(previous_updated_at, base_model.updated_at)
+
+    def test_new_instance_without_kwargs(self):
+        base_model = BaseModel()
+        self.assertIsNotNone(base_model.id)
+        self.assertIsNotNone(base_model.created_at)
+        self.assertIsNotNone(base_model.updated_at)
 
 
 if __name__ == '__main__':
