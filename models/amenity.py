328dc7b7c26576ac2a8caa4046fb1c524d7c891a
T-sosthenese <sosthenetimi94@gmail.com>
2023-07-15 12:01:13 +0300

    more amenities

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,3 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """ Class Amenity inheriting from Class BaseModel."""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """ Initialization of the class."""
-        super().__init__(*args, **kwargs)
