06933f7f6c6f73d66e80dd8181a01cc3e0a487cd
Congo Justus Migue <congomigue@gmail.com>
2023-07-15 12:21:17 +0000

    Updated the test file for console.py

---
+++ tests/test_console.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_emptyline(self, mock_stdout):
+        console = HBNBCommand()
+        self.assertFalse(console.emptyline())
+        self.assertEqual(mock_stdout.getvalue(), '')
+
+    def test_quit_command(self):
+        console = HBNBCommand()
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.assertTrue(console.onecmd('quit'))
+            self.assertEqual(mock_stdout.getvalue(), '')
+
+    def test_create_command(self):
+        console = HBNBCommand()
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.assertFalse(console.onecmd('create'))
+            self.assertIn("** class name missing **", mock_stdout.getvalue())
+
+    def test_show_command(self):
+        console = HBNBCommand()
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.assertFalse(console.onecmd('show'))
+            self.assertIn("** class name missing **", mock_stdout.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
